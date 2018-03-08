5a8faaa618b0253d21fef157ffcfe1a8bbb75f4f
Steve Hunter <steve@smtahunter.com>
2018-03-08 19:08:31 +1100

    Working WeChat mini-program!

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
+    // Show local storage capacity 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    // 登录
+    // log in 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
